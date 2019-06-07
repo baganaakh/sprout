b209fca42d5f815abc507ec64d07accca66d314e
baganaakh <baganaakh@gmail.com>
2019-06-07 19:22:34 +0800

    update

---
+++ Zombie/demo.js
@@ -0,0 +1,16 @@
+baganaa_blink(b_delay, b_interval, b_blick, b_blick_count , b_countdown, b_countdown_count, b_step, b_text2) {	
+//	document.title
+}
+
+baganaa_pulsating(b_delay, b_interval, b_blick, b_blick_count , b_countdown, b_countdown_count, b_step, b_text2 = "") {	
+ 
+ }
+ 
+baganaa_shuffling(b_delay, b_interval, b_blick, b_blick_count , b_countdown, b_countdown_count, b_step, b_text2 = "") {	
+ document.title = 'baganaa';
+ }
+ 
+ 
+baganaa_marquee(b_delay, b_interval, b_blick, b_blick_count , b_countdown, b_countdown_count, b_step, b_text2 = "") {
+	
+}	
